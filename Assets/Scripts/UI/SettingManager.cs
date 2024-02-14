8cc31062c59f7ad7f6826a487260b59b980de79e
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2024-02-14 15:05:45 +0800

    settings panel, extra

--- Assets/Scripts/UI/SettingManager.cs
+++ Assets/Scripts/UI/SettingManager.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class SettingManager : MonoBehaviour
 {
+    public Toggle fullscreenToggle, vsyncToggle;
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fullscreenToggle.isOn = Screen.fullScreen;
+
+        if(QualitySettings.vSyncCount == 0)
+        {
+            vsyncToggle.isOn = false;
+        }
+        else
+        {
+            vsyncToggle.isOn = true;
+        }
+
     }
 
     // Update is called once per frame
@@ -15,4 +31,42 @@ public class SettingManager : MonoBehaviour
     {
         
     }
+
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if(selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1; 
+        }
+    }
+
+    public void ApplySettings()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+
+        if (vsyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
