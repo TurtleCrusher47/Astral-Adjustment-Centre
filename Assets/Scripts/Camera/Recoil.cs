3d6273c13441eeef77f0273d842aa9fa099580b3
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2024-02-15 23:00:00 +0800

    Add `HeadBob` `Recoil` and `WeaponSway` scripts

---
+++ Assets/Scripts/Camera/Recoil.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+    [SerializeField] private float snappiness;
+    [SerializeField] private float returnSpeed;
+    
+    // Rotations
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+   
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        targetRotation  = Vector3.Lerp(targetRotation, Vector3.zero, returnSpeed * Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation, targetRotation, snappiness * Time.fixedDeltaTime);
+        transform.localRotation = Quaternion.Euler(currentRotation);
+    }
+
+    public void GunRecoil(Vector3 recoil)
+    {
+        targetRotation += new Vector3(recoil.x, Random.Range(-recoil.y, recoil.y), Random.Range(-recoil.z, recoil.z));
+    }
+}
