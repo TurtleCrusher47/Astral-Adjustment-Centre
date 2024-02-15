e760b909c9878d9bd844ba0e1209f07dfe051646
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2024-02-15 12:44:52 +0800

    Rogue Buff Animator, Fix cursor hitpoint

---
+++ Assets/Scripts/UI/RogueAnimator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueAnimator : MonoBehaviour
+{
+    public GameObject buffPanel, buffPanel2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RerollButton()
+    {
+
+    }
+}
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -10,6 +10,9 @@ public class UIManager : MonoBehaviour
     [Header("GameObjects")]
     public GameObject optionButton, exitButton;
 
+    [Header("Buff Panels")]
+    public List<GameObject> buffPanels = new List<GameObject>();
+
     private void Start()
     {
         // Reset PhonePanel
@@ -19,6 +22,70 @@ public class UIManager : MonoBehaviour
         // Reset Option Panel
         optionPanel.transform.localPosition = new Vector3(0f, 0f, 0f);
         optionPanel.transform.localScale = new Vector3(0f, 0f, 0f);
+
+        StartBuff();
+
+        ShuffleBuffPanel();
+
+        Debug.Log("GameObjects in List: " + buffPanels.Count);
+
+        foreach( GameObject buffPanel in buffPanels)
+        {
+            Debug.Log("GameObject Name: " + buffPanel.name);
+        }
+    }
+
+    private void StartBuff()
+    {
+        // Reset Buff Panel
+        //buffPanels.transform.localPosition = new Vector3(-344.25f, 61f, 0f);
+        //buffPanel.transform.localScale = new Vector3(1f, 1f, 0f);
+        // buffPanel.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        //buffPanel2.transform.localPosition = new Vector3(388f, 126f, 0f);
+        //buffPanel2.transform.localScale = new Vector3(1f, 1f, 0f);
+        //buffPanel2.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        // Test Buff 
+        //LeanTween.moveLocal(buffPanel, new Vector3(-344.25f, 0f, 0f), .6f).setDelay(0.3f).setEase(LeanTweenType.easeInOutQuart);
+        // LeanTween.rotate(buffPanel, new Vector3(0f, 0f, 0f), .9f).setDelay(1.2f).setEase(LeanTweenType.easeInOutExpo);
+
+        //LeanTween.moveLocal(buffPanel2, new Vector3(384f, 0f, 0f), .6f).setDelay(0.5f).setEase(LeanTweenType.easeInOutQuart);
+        //LeanTween.rotate(buffPanel2, new Vector3(0f, 0f, 0f), .9f).setDelay(1.4f).setEase(LeanTweenType.easeInOutExpo);
+    }
+
+    private void ShuffleBuffPanel()
+    {
+        int n = buffPanels.Count;
+        while ( n > 1)
+        {
+            n--;
+            int k  = Random.Range(0, n + 1);
+            GameObject temp = buffPanels[k];
+            buffPanels[k] = buffPanels[n];
+            buffPanels[n] = temp;
+        }
+    }
+
+    public void RerollButton()
+    {
+        ShuffleBuffPanel();
+
+        // Reset Buff Panel
+        //buffPanel.transform.localPosition = new Vector3(-344.25f, 61f, 0f);
+        //buffPanel.transform.localScale = new Vector3(1f, 1f, 0f);
+        //buffPanel.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        //buffPanel2.transform.localPosition = new Vector3(388f, 126f, 0f);
+        //buffPanel2.transform.localScale = new Vector3(1f, 1f, 0f);
+        //buffPanel2.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        // Test Buff Animations 
+        //LeanTween.moveLocal(buffPanel, new Vector3(-344.25f, 0f, 0f), .6f).setDelay(0.3f).setEase(LeanTweenType.easeInOutQuart);
+        //LeanTween.rotate(buffPanel, new Vector3(0f, 0f, 0f), .9f).setDelay(1.2f).setEase(LeanTweenType.easeInOutExpo);
+
+        //LeanTween.moveLocal(buffPanel2, new Vector3(384f, 0f, 0f), .6f).setDelay(0.5f).setEase(LeanTweenType.easeInOutQuart);
+        //LeanTween.rotate(buffPanel2, new Vector3(0f, 0f, 0f), .9f).setDelay(1.4f).setEase(LeanTweenType.easeInOutExpo);
     }
 
     private void Update()
