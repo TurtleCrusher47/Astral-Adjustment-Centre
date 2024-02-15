c561ee23cf9e222242d390069776095924564910
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2024-02-15 15:07:21 +0800

    Base Class for the Buffs

--- Assets/Scripts/Buffs/Base/BaseBuff.cs
+++ Assets/Scripts/Buffs/Base/BaseBuff.cs
@@ -6,12 +6,17 @@ public class BaseBuff : MonoBehaviour
 {
     // Damage
     public float damageMultiplier { get; set; }
+    public float damageBonus { get; set; }
     // Health
     public float healthMultiplier { get; set; }
+    public float healthBonus { get; set; }
     // Movement Speed
     public float movementMultiplier { get; set; }
+    public float movementBonus { get; set; }
     // Melee Attack Speed
     public float atkSpdMultiplier { get; set; }
+    public float atkSpdBonus { get; set; }
     // Ranged Fire Rate
     public float firerateMultiplier { get; set; }
+    public float firerateBonus { get; set;}
 }
