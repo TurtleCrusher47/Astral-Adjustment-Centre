61ec79e5f857da83bc5c94589183d96d3ea35e1b
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-02-18 17:55:57 +0800

    Add function to load next level

---
+++ Assets/Scripts/World/Map/ElevatorTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorTrigger : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        // load next level
+        Debug.Log("Next Level");
+    }
+}
