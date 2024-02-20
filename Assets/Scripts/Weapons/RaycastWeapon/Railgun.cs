cb2c8f2f59f3d13cf9bfea463cf7ce7a1d383739
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2024-02-20 23:51:34 +0800

    Create Railgun secondary attack and modify `RenderTraceLine`

--- Assets/Scripts/Weapons/RaycastWeapon/Railgun.cs
+++ Assets/Scripts/Weapons/RaycastWeapon/Railgun.cs
@@ -1,39 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Railgun : RaycastRangedWeapon
 {
-    private float chargeMultiplier = 0.25f;
+    private float chargeMultiplier = 1.25f;
+    private float elapsedTime = 0;
 
     protected override void UseSecondary()
     {
+        // elapsed time to check per second
         if (rangedWeaponData.currentAmmo - 10 >= 0)
         {
-            chargeMultiplier += Time.deltaTime;
-            rangedWeaponData.currentAmmo -= 10;
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= 1f)
+            {
+                elapsedTime %= 1f;
+                chargeMultiplier += 1;
+                rangedWeaponData.currentAmmo -= 10;
+            }
+        }
+        // If player starts reloading while charging
+        if (rangedWeaponData.reloading)
+        {
+            ResetValues();
         }
     }
 
     protected override void UseSecondaryUp()
     {
-        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, raycastProjectileData.maxDistance, targetLayers))
+        // To store the variable that was furthest
+        RaycastHit furthestHit;
+
+        // Do not do damage if the player did not properly charge
+        if (chargeMultiplier <= 1.25f)
         {
-            StartCoroutine(RenderTraceLine(hitInfo.point));
-            Debug.Log(hitInfo.transform.name);
-            IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
-            damageable?.Damage(raycastProjectileData.damage * chargeMultiplier);
-            
-            // GameObject effect = ObjectPoolManager.SpawnObject(hitEffect, hitInfo.point, hitInfo.transform.rotation);
-            // Destroy(effect, 0.5f);
+            ResetValues();
+            return;
         }
-        else
+        RaycastHit[] hits = Physics.RaycastAll(cam.position, cam.forward, raycastProjectileData.maxDistance, targetLayers);
+        // Assign furthestHit to the first point in hits
+        furthestHit = hits[0];
+        for (int i = 0; i < hits.Length; i++)
         {
-            StartCoroutine(RenderTraceLine(cam.forward * raycastProjectileData.maxDistance));
+            // If the new point is further away than the old point
+            if (Vector3.Distance(firePoint.position, hits[i].point) >= Vector3.Distance(firePoint.position, furthestHit.point))
+            {
+                furthestHit = hits[i];
+            }
+
+            if (hits[i].collider.TryGetComponent<IDamageable>(out var damageable))
+            {
+                damageable.Damage(raycastProjectileData.damage * chargeMultiplier);
+            }
+            Debug.Log(hits[i].transform.name);
+
+            // If it is the last object
+            if (i == hits.Length - 1)
+            {
+                StartCoroutine(RenderTraceLine(furthestHit.point, 1));
+            }
         }
+
+        ResetValues();
 
         // Debug.Log("RB Up");
         OnSecondary();
+    }
+
+    private void ResetValues()
+    {
+        elapsedTime = 0;
+        chargeMultiplier = 1.25f;
     }
 
     protected override void UseAbility()
