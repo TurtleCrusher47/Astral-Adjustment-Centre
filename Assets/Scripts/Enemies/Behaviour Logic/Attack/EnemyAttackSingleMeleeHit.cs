ad8a3d4f89d106ac559f7b1a7cfa8fa6eca51453
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-02-21 18:40:16 +0800

    Fix some enemy bugs Add attack indicator for melee enemy

--- Assets/Scripts/Enemies/Behaviour Logic/Attack/EnemyAttackSingleMeleeHit.cs
+++ Assets/Scripts/Enemies/Behaviour Logic/Attack/EnemyAttackSingleMeleeHit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Attack- Single Melee Hit", menuName = "Enemy Logic/Attack State/Single Melee Hit")]
@@ -12,6 +13,7 @@ public class EnemyAttackSingleMeleeHit : EnemyAttackSOBase
     [SerializeField] private float _distanceToCountExit = 4.5f;
 
     private Animator animator;
+    private MeleeEnemy self;
 
 
     private float _exitTimer;
@@ -21,6 +23,7 @@ public class EnemyAttackSingleMeleeHit : EnemyAttackSOBase
     {
         base.DoEnterLogic();
         animator = enemy.gameObject.GetComponent<Animator>();
+        self = transform.GetComponent<MeleeEnemy>();
     }
 
     public override void DoExitLogic()
@@ -49,8 +52,12 @@ public class EnemyAttackSingleMeleeHit : EnemyAttackSOBase
             //trigger animator to play punch animation
             //set gauntlet trigger collider on and off in animation
 
+            self.indicator.SetActive(true);
             animator.SetTrigger("isPunch");
-
+        }
+        if (_timer > 1.0f && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            self.indicator.gameObject.SetActive(false);
         }
 
         if (Vector3.Distance(playerTransform.position, enemy.transform.position) > (_distanceToCountExit * playerTransform.localScale.x))
