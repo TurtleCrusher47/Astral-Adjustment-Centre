d73ae6e078f30f7770a9fadbb6102a03be2ba916
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2024-02-22 11:24:20 +0800

    Change `GameObjectProjectileData` to take in float instead of int

--- Assets/Scripts/Projectiles/EggProjectile.cs
+++ Assets/Scripts/Projectiles/EggProjectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EggProjectile : GameObjectProjectile
 {
+    [SerializeField] float explosiveTimer = 3;
     private bool isTriggered = false;
 
     protected override void OnEnable()
@@ -17,5 +18,34 @@ public class EggProjectile : GameObjectProjectile
     {
         if (collider.gameObject.CompareTag("PlayerCollider") || collider.gameObject.CompareTag("WeaponCollider"))
         return;
+
+        if (collider.transform.TryGetComponent<IDamageable>(out IDamageable damageable))
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        // Check if it has already exploded
+        if (!isTriggered)
+        {
+            foreach (var collider in Physics.OverlapSphere(transform.position, 3))
+            {
+                if (collider.TryGetComponent<IDamageable>(out var damageable))
+                {
+                    damageable.Damage(gameObjectProjectileData.damage);
+                }
+            }
+
+            StartCoroutine(ObjectPoolManager.Instance.ReturnObjectToPool(this.gameObject));
+        }
+    }
+
+    private IEnumerator StartExplosiveTimer()
+    {
+        yield return new WaitForSeconds(explosiveTimer);
+
+        Explode();
     }
 }
