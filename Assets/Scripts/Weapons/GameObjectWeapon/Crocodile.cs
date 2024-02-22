f2494803ffc598111808c8a4ae62464871ffd42c
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2024-02-22 10:30:08 +0800

    Create crocodile prefab

---
+++ Assets/Scripts/Weapons/GameObjectWeapon/Crocodile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodile : GameObjectRangedWeapon
+{
+    [SerializeField] GameObject grenadePrefab;
+
+    protected override void OnAbility()
+    {
+    }
+
+    protected override void OnSecondary()
+    {
+    }
+
+    protected override void UseAbility()
+    {
+    }
+
+    protected override void UseSecondary()
+    {
+    }
+}
