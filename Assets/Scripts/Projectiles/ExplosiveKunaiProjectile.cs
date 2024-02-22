6d2644f162b8392f8f34b02d27fef418a0430825
Bernard Ng <bernardngboonhao@gmail.com>
2024-02-22 22:55:42 +0800

    Add SFXs
    - Impact for ShurikenProjectile
    - Explosion for ExplosiveKunai
    - Explosion for BarrelTrap

--- Assets/Scripts/Projectiles/ExplosiveKunaiProjectile.cs
+++ Assets/Scripts/Projectiles/ExplosiveKunaiProjectile.cs
@@ -32,6 +32,8 @@ public class ExplosiveKunaiProjectile : GameObjectProjectile
             if (collider.TryGetComponent<IDamageable>(out var damageable))
             {
                 damageable.Damage(gameObjectProjectileData.damage * GetAtkMultiplier());
+
+                AudioManager.Instance.PlaySFX("SFXExplosion");
             }
         }
         
