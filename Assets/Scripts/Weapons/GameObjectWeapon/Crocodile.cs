541151cff53315960b2df47a002c2c88482511cb
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2024-02-22 12:53:47 +0800

    Create crocodile

--- Assets/Scripts/Weapons/GameObjectWeapon/Crocodile.cs
+++ Assets/Scripts/Weapons/GameObjectWeapon/Crocodile.cs
@@ -4,17 +4,63 @@ using UnityEngine;
 
 public class Crocodile : GameObjectRangedWeapon
 {
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] float lazerRange;
+    private float elapsedTime = 0;
+
+    protected override void UseSecondary()
+    {
+        // If player starts reloading while charging
+        if (rangedWeaponData.reloading)
+        {
+            elapsedTime = 0;
+        }
+        else if (rangedWeaponData.currentAmmo - 1 >= 0)
+        {
+            if (Physics.Raycast(cam.position, camRotation.forward, out RaycastHit hitInfo, lazerRange, targetLayers))
+            {
+                elapsedTime += Time.deltaTime;
+                if (elapsedTime >= 1f)
+                {
+                    elapsedTime %= 1f;
+                    rangedWeaponData.currentAmmo -= 1;
+
+                    Debug.Log(hitInfo.transform.name);
+                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
+                    damageable?.Damage(gameObjectProjectileData.damage);
+                    
+                    // GameObject effect = ObjectPoolManager.SpawnObject(hitEffect, hitInfo.point, hitInfo.transform.rotation);
+                    // Destroy(effect, 0.5f);
+                }
+                RenderLine(hitInfo.point);
+            }
+        }
+    }
+
+    protected override void UseSecondaryUp()
+    {
+        elapsedTime = 0;
+        lineRenderer.positionCount = 0;
+    }
+
     protected override void OnSecondary()
     {
+        
+    }
+
+    private void RenderLine(Vector3 hitPosition)
+    {
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, firePoint.position);
+        lineRenderer.SetPosition(1, hitPosition);
+
+        Debug.Log("Rendering Line");
     }
 
     protected override void OnAbility()
     {
     }
 
-    protected override void UseSecondary()
-    {
-    }
 
     protected override void UseAbility()
     {
